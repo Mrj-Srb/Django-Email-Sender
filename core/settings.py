5f5150499e1d143e69df9bb5e269189ac153f4fb
Meraj Sarab <merajpython@gmail.com>
2024-03-12 23:13:30 +0330

    Configs for sending email

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email configs
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "your email" #sender's email-id
+EMAIL_HOST_PASSWORD = "your app password" #password associated with above email-id
+
